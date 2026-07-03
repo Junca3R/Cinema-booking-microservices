077a3231979ef2797eb67067b14ce7e79a22fc66
Oscar Junca <oscardavidjunca000@gmail.com>
2026-07-03 11:15:52 -0500

    Primer commit

---
+++ ms-cinema-catalog/src/main/java/com/cinema/catalog/MsCinemaCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.cinema.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCinemaCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCinemaCatalogApplication.class, args);
+	}
+
+}
---
+++ ms-cinema-catalog/src/test/java/com/cinema/catalog/MsCinemaCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinema.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCinemaCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
