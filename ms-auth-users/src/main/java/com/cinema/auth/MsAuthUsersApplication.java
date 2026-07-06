2a6cbc9a7c0eb0deccfc80679592e00a104acf3e
Oscar Junca <oscardavidjunca000@gmail.com>
2026-07-06 12:19:48 -0500

    Initial Commit for auth and user microservice

---
+++ ms-auth-users/src/main/java/com/cinema/auth/MsAuthUsersApplication.java
@@ -0,0 +1,13 @@
+package com.cinema.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuthUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAuthUsersApplication.class, args);
+	}
+
+}
---
+++ ms-auth-users/src/test/java/com/cinema/auth/MsAuthUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinema.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuthUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
